2854a3064fa8d02d84025dfbc1b9359f03485775
Kellen Proctor <kellenproctor@gmail.com>
2017-03-09 15:46:29 -0800

    feat: updated filename references

--- code/data_converter.py
+++ code/data_converter.py
@@ -118,7 +118,7 @@ def test():
     # NOTE: if you are running this code on your computer, with a larger dataset, 
     # call the process_map procedure with pretty=False. The pretty=True option adds 
     # additional spaces to the output, making it significantly larger.
-    data = process_map('./data/la-small.osm')
+    data = process_map('../data/la-small.osm')
     pprint.pprint(data)
 
 if __name__ == "__main__":
