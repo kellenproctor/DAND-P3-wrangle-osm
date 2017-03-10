3ba18f4af8ccf0be112f674c65f992280fbd8a48
Kellen Proctor <kellenproctor@gmail.com>
2017-03-09 22:54:08 -0800

    feat: added a few queries

--- query.py
+++ query.py
@@ -11,6 +11,11 @@ def get_db(db_name):
     db = client[db_name]
     return db
 
+# Create a limit query
+def limit():
+    limit = {"$limit" : 1}
+    return limit
+
 # Create a sort query on count
 def sort():
     sort = {"$sort" : {"count" : -1}}
@@ -18,18 +23,28 @@ def sort():
 
 # Create a aggregation of tag types
 def agg_types():
-    group = {"$group" : {"_id" : "$type", "count" : {"$sum" : 1}}}
+    group = {"$group" : {"_id" : "$tag_type",
+                        "count" : {"$sum" : 1}}}
+    return group
+
+# Create a group for all user entries
+def top_user():
+    group = {"$group" : {"_id" : "$created.user",
+                        "count" : {"$sum" : 1}}}
     return group
 
 # Commands dictionary for converting string to function name
 COMMANDS = {
+    "limit" : limit,
     "sort" : sort,
-    "agg_types" : agg_types
+    "agg_types" : agg_types,
+    "top_user" : top_user
 }
 
 # Take the list of queries and make a query pipeline
-def make_pipeline(list):
-    # Complete the aggregation pipeline
+def make_pipeline(args):
+    # Complete the aggregation pipeline by iterating through the list of query
+    # functions - args.
     pipeline = []
     for arg in args:
         pipeline.append(COMMANDS[arg]())
