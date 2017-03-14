e5cfe9645f93f3e165706789fb62db164168607d
Kellen Proctor <kellenproctor@gmail.com>
2017-03-14 13:13:13 -0700

    file: add cleaning.py

---
+++ cleaning.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+For running queries against the MongoDB osm database la collection
+"""
+
+def check_city(city_name):
+    return
+
+if __name__ == '__main__':
+    pass
+
